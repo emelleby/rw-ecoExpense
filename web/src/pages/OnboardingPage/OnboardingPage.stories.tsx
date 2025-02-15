ce7efb2aa99ae28e2db49fd3003b8e942e6b4321
Eivind Melleby <emelleby@gmail.com>
2025-02-15 01:35:25 +0100

    Update OrganizationCell mock data and stories; add user mock for OnboardingPage and remove LoginPage stories

--- web/src/pages/OnboardingPage/OnboardingPage.stories.tsx
+++ web/src/pages/OnboardingPage/OnboardingPage.stories.tsx
@@ -1,13 +1,41 @@
+import { ClerkProvider } from '@clerk/clerk-react'
 import type { Meta, StoryObj } from '@storybook/react'
+
+import { routes } from '@redwoodjs/router'
+import { mockCurrentUser } from '@redwoodjs/testing/web'
 
 import OnboardingPage from './OnboardingPage'
 
 const meta: Meta<typeof OnboardingPage> = {
   component: OnboardingPage,
+  decorators: [
+    (Story) => {
+      mockCurrentUser({
+        id: 'user_2pkZyi796FvxrYZlUpFeddb3C0e',
+        dbUserId: 4,
+        organizationId: 8,
+        roles: ['admin', 'superuser'],
+        metadata: {
+          organizationId: 8,
+
+          roles: ['admin', 'superuser'],
+        },
+      })
+
+      // Mock the routes properly
+      routes.login = () => '/login'
+      routes.homey = () => '/homey'
+
+      return (
+        <ClerkProvider publishableKey="pk_test_cHJpbWUtYW5lbW9uZS01LmNsZXJrLmFjY291bnRzLmRldiQ">
+          <Story />
+        </ClerkProvider>
+      )
+    },
+  ],
 }
 
 export default meta
 
 type Story = StoryObj<typeof OnboardingPage>
-
 export const Primary: Story = {}
